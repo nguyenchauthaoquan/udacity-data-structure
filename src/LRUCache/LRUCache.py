f889b970d9f1fa69e84c841f1704f0dde92963e6
nguyenchauthaoquan <QuanNCT@outlook.com>
2024-02-16 15:42:36 +0700

    Implement the LRU cache algorithm

--- src/LRUCache/LRUCache.py
+++ src/LRUCache/LRUCache.py
@@ -1,4 +1,4 @@
-from src.LRUCache import DoublyLinkedList, Node
+from src.LRUCache import DoublyLinkedList
 
 
 class LRUCache:
@@ -24,15 +24,18 @@ class LRUCache:
             self.linked_list.poll()
 
 
-our_cache1 = LRUCache(5)
-our_cache1.set(1, 1)
-our_cache1.set(2, 2)
-our_cache1.set(3, 3)
-our_cache1.set(4, 4)
-print(our_cache1.get(1))  # returns 1
-print(our_cache1.get(2))  # returns 2
-print(our_cache1.get(9))  # returns -1 because 9 is not present in the cache
-our_cache1.set(5, 5)
-our_cache1.set(6, 6)
-# returns -1 because the cache reached it's capacity and 3 was the LRU entry
-print(our_cache1.get(3))
+our_cache = LRUCache(5)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+
+print(our_cache.get(1))  # returns 1
+print(our_cache.get(2))  # returns 2
+print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
+
+our_cache.set(5, 5)
+our_cache.set(6, 6)
+
+print(our_cache.get(3))  # returns -1 because the cache reached its capacity and 3 was the least recently used entry
