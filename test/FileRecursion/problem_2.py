de6fdb2b5d04c78034c06238c99e192d1c30213c
nguyenchauthaoquan <QuanNCT@outlook.com>
2024-03-18 17:06:56 +0700

    update file recursion test cases

--- test/FileRecursion/problem_2.py
+++ test/FileRecursion/problem_2.py
@@ -11,44 +11,79 @@ class MyTestCase(unittest.TestCase):
         """
         super(MyTestCase, self).__init__(*args, **kwargs)
         self.file_rec = FileRecursion()
-        self.path = os.getcwd() + '/testdir'
+        self.absolute_path = os.getcwd() + '/testdir'
+        self.relative_path = './testdir'
 
     def test_edge_case_1(self):
         """
-        Tests the File Recursion with the auto-generated path
+        Tests the File Recursion for absolute file path case
         """
-        self.assertEqual(self.file_rec.find_files(suffix='a', path=self.path), set())
+        self.assertEqual(self.file_rec.find_files(suffix='a', path=self.absolute_path), set())
 
     def test_edge_case_2(self):
+        self.assertEqual(self.file_rec.find_files(suffix='c', path=self.absolute_path), {os.getcwd() + '/testdir\\subdir1\\a.c',
+                                                                                os.getcwd() + '/testdir\\subdir3\\subsubdir1\\b.c',
+                                                                                os.getcwd() + '/testdir\\subdir5\\a.c',
+                                                                                os.getcwd() + '/testdir\\t1.c'})
+
+    def test_edge_case_3(self):
+        """
+         Tests the File Recursion for absolute file path case
+        """
+        self.assertEqual(self.file_rec.find_files(suffix='h', path=self.absolute_path), {os.getcwd() + '/testdir\\subdir1\\a.h',
+                                                                                  os.getcwd() + '/testdir\\subdir3\\subsubdir1\\b.h',
+                                                                                  os.getcwd() + '/testdir\\subdir5\\a.h',
+                                                                                  os.getcwd() + '/testdir\\t1.h'})
+
+    def test_edge_case_4(self):
         """
         Tests the File Recursion for relative file path case
         """
-        self.assertEqual(self.file_rec.find_files(suffix='c', path='./testdir'), {'./testdir\\subdir1\\a.c',
+        self.assertEqual(self.file_rec.find_files(suffix='a', path=self.relative_path), set())
+
+    def test_edge_case_5(self):
+        """
+        Tests the File Recursion for relative file path case
+        """
+        self.assertEqual(self.file_rec.find_files(suffix='c', path=self.relative_path), {'./testdir\\subdir1\\a.c',
                                                                                   './testdir\\subdir3\\subsubdir1\\b.c',
                                                                                   './testdir\\subdir5\\a.c',
                                                                                   './testdir\\t1.c'})
 
-    def test_edge_case_3(self):
+    def test_edge_case_6(self):
         """
         Tests the File Recursion for relative file path case
         """
-        self.assertEqual(self.file_rec.find_files(suffix='h', path='./testdir'), {'./testdir\\subdir1\\a.h',
+        self.assertEqual(self.file_rec.find_files(suffix='h', path=self.relative_path), {'./testdir\\subdir1\\a.h',
                                                                                   './testdir\\subdir3\\subsubdir1\\b.h',
                                                                                   './testdir\\subdir5\\a.h',
                                                                                   './testdir\\t1.h'})
 
     def test_empty_suffix_case_1(self):
         """
-        Tests the File Recursion for empty suffix
+        Tests the File Recursion for empty suffix with absolute path.
         """
-        self.assertEqual(self.file_rec.find_files(suffix='', path=self.path), [])
+        self.assertEqual(self.file_rec.find_files(suffix='', path=self.absolute_path), [])
+
+    def test_empty_suffix_case_2(self):
+        """
+        Tests the File Recursion for empty suffix with absolute path.
+        """
+        self.assertEqual(self.file_rec.find_files(suffix='', path=self.relative_path), [])
 
     def test_file_not_found_case_1(self):
         """
-        Tests the File Recursion for file not found
+        Tests the File Recursion for file not found with relative directory
         """
         with self.assertRaises(FileNotFoundError):
             self.file_rec.find_files(suffix='c', path="./abcd")
+
+    def test_file_not_found_case_2(self):
+        """
+        Tests the File Recursion for file not found with absolute directory
+        """
+        with self.assertRaises(FileNotFoundError):
+            self.file_rec.find_files(suffix='c', path=os.getcwd() + "/abcd")
 
 
 if __name__ == '__main__':
